2e6ce51aef1cb559727c0a0d803d9a6dfe317716
UgaldeZarzaRaul <al222310599@gmail.com>
2024-11-11 18:32:06 -0600

    Proyecto en git

---
+++ resources/views/home.blade.php
@@ -0,0 +1,31 @@
+@extends('adminlte::page')
+
+@section('content')
+
+@section('title','Viedeogames Universie')
+
+@section('content_header')
+<h1>Tienda de Videojuegos</h1>
+@stop
+
+@section('content')
+<div class="card">
+    <div class="card-header">
+        <h1 class="card-title">seleccion de videojuegos</h1>
+    </div>
+    <div class="card-body">
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Modi voluptates, porro libero voluptas nobis dolores reprehenderit quos, sequi deleniti commodi ipsum quas laborum soluta, illo neque provident. Recusandae, harum dolor.</p>
+    </div>
+</div>
+
+@stop
+
+@section('css')
+<linl rel="stylesheet" href="/css/admin_custom.css">
+@stop
+
+@section('js')
+<script>console.log('hola'); </script>
+@stop
+
+@endsection
